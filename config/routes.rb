e932ccbce1b977fa4370a1e002106b29de8b75da
Jose Soto <jsoto7087@gmail.com>
2023-04-19 23:26:45 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-
+  resources :user, only:[:show, :create]
 
   post "/login", to: "sessions#create"
-    get "/auth", to: "users#show"
+  get "/auth", to: "users#show"
 
-    get "/categories", to: "categories#show"
-    get "/expenses", to: "expenses#show"
+  get "/categories", to: "categories#show"
+  get "/expenses", to: "expenses#show"
+  
 end
